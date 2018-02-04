3af95364bc947268f9ab30eb6fe2598d644601c3
Scott Sanders <jssjr@github.com>
2018-02-04 18:09:51 -0500

    fixes

--- Marlin/PrintrBotConfig.h
+++ Marlin/PrintrBotConfig.h
@@ -1,5 +1,7 @@
-#define ZROD_FOUR_START_8MM 1
+/** @jssjr's home printer config **/
+
+#define ZROD_QUARTER 1
 #define PULLEY_XY_16T 1
-#define EXTRUDER_GEARHEAD 1
-#define X_AXIS_STANDARD 1
-#define ZROD_SUPER_Z 1
+#define EXTRUDER_OLD 1
+#define X_AXIS_UPGRADE 1
+#define ZROD_STANDARD 1
